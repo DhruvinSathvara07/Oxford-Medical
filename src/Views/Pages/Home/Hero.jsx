fc535114954323ce119f8db121776e16a334de59
Dhruvin <dhruvinsathvara05@gmail.com>
2025-06-19 18:53:07 +0530

    Make a hero section dynamic

--- src/Views/Pages/Home/Hero.jsx
+++ src/Views/Pages/Home/Hero.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 const Slider = () => {
+  const [herodata, setHerodata] = useState();
+
+  const fetchingData = async () => {
+    try {
+      const response = await axios.get("http://localhost:1000/hero");
+      setHerodata(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchingData();
+  }, []);
+
+  const herosectionData =
+    Array.isArray(herodata) && herodata.length > 0 ? herodata[0] : {};
+  const heroStyle = {
+    background: herosectionData.hero_img
+      ? `url(http://localhost:1000/uploads/${herosectionData.hero_img}) no-repeat center center`
+      : "No Image",
+    minHeight: "400px",
+  };
+
   return (
     <div className="slider-container">
       <div className="slider-hero">
@@ -12,32 +37,30 @@ const Slider = () => {
                 style={{ color: "#E4040E" }}
               ></i>
             ))}
-            <span className="rating-text">
-              For over 15 years Oxford Medical Instruments Ltd.
-            </span>
+            <span className="rating-text">{herosectionData.rating_text}</span>
           </div>
 
           <div className="hero-text">
-            <h2>Get 20% Discount of</h2>
-            <h2>Breathing Salt Therapy</h2>
-            <h2>Inhalers</h2>
+            <h2>{herosectionData.hero_text1}</h2>
+            <h2>{herosectionData.hero_text2}</h2>
+            <h2>{herosectionData.hero_text3}</h2>
             <p>
-              Salt Therapy Inhalers naturally support easier breathing by
+              {herosectionData.hero_para1}
               <br />
-              delivering fine salt particles to your airways.
+              {herosectionData.hero_para2}
             </p>
-            <span className="price-info">Starting at $40</span>
+            <span className="price-info">{herosectionData.price_info}</span>
           </div>
 
           <div className="button-section ms-0 ps-0 ms-lg-5 ps-lg-5">
             <a href="#" className="shop-button">
-              Shop Now
+              {herosectionData.btn_text}
             </a>
-            <span className="genuine-text">100% genuine Products</span>
+            <span className="genuine-text">{herosectionData.text}</span>
           </div>
         </div>
 
-        <div className="slider-right"></div>
+        <div className="slider-right" style={heroStyle}></div>
       </div>
     </div>
   );
